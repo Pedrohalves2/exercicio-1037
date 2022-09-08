ce31bc69fdfad358fae71a8adef51d739fb9ae14
Pedrohalves2 <108554576+Pedrohalves2@users.noreply.github.com>
2022-09-08 20:22:08 -0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+            int A, B, C, D;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+            if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0)
+            {
+                Console.WriteLine(" Valores aceitos ");
+            }
+            else
+            {
+                Console.WriteLine("valores nao aceitos");
+            }
+            Console.ReadLine();
+        }
+    }
+}
